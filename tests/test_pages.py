dd3948f0e06f9477cf7bf7c95a66b6114272a98e
Kun <kunlong.wu@gmail.com>
2026-06-17 17:45:23 +0800

    Fix ICS download link target

--- tests/test_pages.py
+++ tests/test_pages.py
@@ -5,5 +5,6 @@ def test_calendar_subscription_ui_uses_webcal_for_subscribe_and_https_for_downlo
     page = calendars_page()
 
     assert 'href="${urls.webcal_url}">Subscribe to Apple Calendar' in page
-    assert 'href="${urls.https_url}">Download ICS' in page
+    assert 'function downloadUrl(urls)' in page
+    assert 'href="${downloadUrl(urls)}">Download ICS' in page
     assert 'data-url="${esc(urls.https_url)}">Copy' in page
